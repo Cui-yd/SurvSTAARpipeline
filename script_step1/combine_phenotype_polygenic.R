978a4999fbd61d22a9cd0634e1d6aec189d02d0f
yidancui <amber_cui@qq.com>
2025-07-18 09:53:01 +0800

    modify combine code

--- script_step1/combine_phenotype_polygenic.R
+++ script_step1/combine_phenotype_polygenic.R
@@ -10,45 +10,69 @@ sample = sample[order(sample$IID),]
 covariate = fread("./path/to/covariates.txt")
 covariate = covariate[order(covariate$IID),]
 
+
+
+################################################################
+## If your ID does not contain underscores "_", use this:
 FID_temp = IID_temp = c()
 for (i in 1:nrow(polygenic)) {
   split_name <- strsplit(rownames(polygenic)[i], "_")[[1]]
   FID_temp[i] <- split_name[1]
   IID_temp[i] <- split_name[2]
 }
-
-# Convert to numeric only if you're sure they're all numeric
-if (is.numeric(sample$FID) && is.numeric(sample$IID)) {
-  FID_temp <- as.numeric(FID_temp)
-  IID_temp <- as.numeric(IID_temp)
+## If your ID has a fixed length, use this:
+FID_temp = IID_temp = c()
+ID_length = nchar(sample$IID[1])
+for (i in 1:nrow(polygenic)) {
+  split_name <- strsplit(rownames(polygenic)[i], "_")[[1]]
+  FID_temp[i] <- substr(rownames(polygenic)[i], 1, ID_length)
+  IID_temp[i] <- substr(rownames(polygenic)[i], ID_length + 2, nchar(rownames(polygenic)[i]))
 }
+## If neither of the above applies, you may need to write custom code to split FID and IID
+################################################################
 
-# Check if they're identical (not just equal)
+
+
+## Check if they're identical
 if (identical(FID_temp, sample$FID) && identical(IID_temp, sample$IID)) {
   print(head(polygenic))
   print(head(sample))
   print(head(covariate))
 } else {
-  # Your reordering code
   polygenic <- polygenic[match(sample$IID, IID_temp), ]
-
-  # Redo FID/IID extraction with corrected logic
-  FID_temp = IID_temp = c()
-  for (i in 1:nrow(polygenic)) {
-    split_name <- strsplit(rownames(polygenic)[i], "_")[[1]]
-    FID_temp[i] <- split_name[1]
-    IID_temp[i] <- split_name[2]
-  }
-
-  # Same check as before
-  if (identical(FID_temp, sample$FID) && identical(IID_temp, sample$IID)) {
-    print(head(polygenic))
-    print(head(sample))
-    print(head(covariate))
-  }
 }
 
-### make sure three data have the same ID
+
+
+###################### check again #############################
+## If your ID does not contain underscores "_", use this:
+FID_temp = IID_temp = c()
+for (i in 1:nrow(polygenic)) {
+  split_name <- strsplit(rownames(polygenic)[i], "_")[[1]]
+  FID_temp[i] <- split_name[1]
+  IID_temp[i] <- split_name[2]
+}
+## If your ID has a fixed length, use this:
+FID_temp = IID_temp = c()
+ID_length = nchar(sample$IID[1])
+for (i in 1:nrow(polygenic)) {
+  split_name <- strsplit(rownames(polygenic)[i], "_")[[1]]
+  FID_temp[i] <- substr(rownames(polygenic)[i], 1, ID_length)
+  IID_temp[i] <- substr(rownames(polygenic)[i], ID_length + 2, nchar(rownames(polygenic)[i]))
+}
+## If neither of the above applies, you may need to write custom code to split FID and IID
+################################################################
+
+
+
+## Final check to ensure the polygenic data and phenotype data are correctly matched
+## Make sure three data have the same FID and IID
+if (identical(FID_temp, sample$FID) && identical(IID_temp, sample$IID)) {
+  print(head(polygenic))
+  print(head(sample))
+  print(head(covariate))
+}
+
 
 phenotype_all = cbind(sample, covariate[, 3:ncol(covariate)], polygenic)
 colnames(phenotype_all)[(ncol(sample)+ncol(covariate)-1) : ncol(phenotype_all)] = paste0("Chr", 1:23)
